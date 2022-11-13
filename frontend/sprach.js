28068bb5174799710143ba53647391d9f9019e35
dimicoetvincam <lesamuel2016@gmail.com>
2022-11-13 18:08:58 +0100

    aszgtzasf

--- frontend/sprach.js
+++ frontend/sprach.js
@@ -1,16 +1,22 @@
-var speechkitElement = new webkitSpeechRecognition();
-
+var speechElement = new webkitSpeechRecognition();
 speechElement.lang = 'de-DE';
-speechElement.lang = 'en-EN';
 speechElement.interimResults = true;
 speechElement.continuous = true;
 var final_transcript = '';
 speechElement.start();
 
-speechElement.onstart = function(){
-    console.log('start')
+speechElement.onstart = function() {
 }
 
-speechElement.onresult = function(event){
-    console.log("event")
+speechElement.onresult = function(event) {
+	var interim_transcript = '';
+	for(var i = event.resultIndex; i < event.results.length; ++i) {
+		if(event.results[i].isFinal) {
+			final_transcript += event.results[i][0].transcript;
+		} else {
+			interim_transcript += event.results[i][0].transcript;
+		}
+	}
+	document.getElementById('final').innerHTML = final_transcript;
+	document.getElementById('interim').innerHTML = interim_transcript;
 }
